48669a7d493627692e98a58d7fe34ab3684340eb
Luis Fonseca <ldiego2601@gmail.com>
2022-04-27 21:11:47 +0000

    Modified source and version of aws s3 bucket module.

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,6 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "app.terraform.io/lufonsec-cdw-training/s3-bucket/aws"
+  version = "2.8.0"
 
   bucket_prefix = var.prefix
   acl    = "private"
